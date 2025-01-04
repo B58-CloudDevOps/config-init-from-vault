9d2030b4d17e1ffc1f9228f620ca611ab7f18ef9
CodingManoj <CodingManoj@gmail.com>
2025-01-03 20:56:48 -0500

    Containerizing Vault Init Container

--- run.sh
+++ run.sh
@@ -10,5 +10,18 @@ if [ -z "$VAULT_ADDR" ]; then
     exit 2
 fi 
 
-vault login -tls-skip-verify true &> /dev/null 
-tail -f /dev/null                               # This ensure you pod is streaming the logs and the container will wait reading the logs and given us an opportunity to enter in to the pod
+if [ -z "$ENV" ]; then 
+    echo ENV variable is missing
+    exit 3
+fi 
+
+if [ -z "$APP_NAME" ]; then 
+    echo VAULT_ADDR variable is missing
+    exit 4
+fi 
+
+vault login -tls-skip-verify true &> /dev/null                             # This ensure you pod is streaming the logs and the container will wait reading the logs and given us an opportunity to enter in to the pod
+
+vault kv get roboshop-${ENV}/${APP_NAME} | sed -n '/Data/, $ p'| sed -e '1,3 d' | awk '{print  "export "$1"="$2""}'  > /data/secrets
+
+tail -f /dev/null   
